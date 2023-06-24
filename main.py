bef2bef600fc9db4bfb05453d065a99431c30637
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-06-24 23:24:07 +0200

    Added cors and routes for bindigns

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 from database_handle.database import engine
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from database_handle.models import texts, audios, categories, bindings
-from routes import categories as r_categories, texts as r_texts, audios as r_audios
+from routes import (
+    categories as r_categories,
+    texts as r_texts,
+    audios as r_audios,
+    bindings as r_bindings,
+)
 
 texts.Base.metadata.create_all(engine)
 audios.Base.metadata.create_all(engine)
 categories.Base.metadata.create_all(engine)
 bindings.Base.metadata.create_all(engine)
+origins = "https?://localhost:.+"
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origin_regex=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(r_categories.router)
 app.include_router(r_texts.router)
 app.include_router(r_audios.router)
+app.include_router(r_bindings.router)
 
 
 @app.get("/")
