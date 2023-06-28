8524c93e0520aa8334380c74e3b9a35fbea6cca2
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-06-28 22:09:45 +0200

    Created get_db to be dependency

--- database_handle/database.py
+++ database_handle/database.py
@@ -9,6 +9,14 @@ engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
-SessionLocal = sessionmaker(engine)
+SessionLocal = sessionmaker(bind=engine, autoflush=False)
 
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
