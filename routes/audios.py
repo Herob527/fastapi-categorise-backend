c01e59d163b9fccc36c9d4e1c652e3861a2737b6
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-06-28 22:09:05 +0200

    Adjustments and implemented posting new audio

--- routes/audios.py
+++ routes/audios.py
@@ -1,4 +1,18 @@
-from fastapi import APIRouter
+from typing import Annotated
+from fastapi import APIRouter, Depends, Form, HTTPException, UploadFile
+import librosa
+from io import BytesIO
+from pydantic import UUID4
+
+from sqlalchemy.orm import Session
+from database_handle.database import get_db
+from database_handle.models.audios import Audios
+from database_handle.queries.audios import create_audio
+
+
+class NotAnAudio(Exception):
+    pass
+
 
 __all__ = ["router"]
 
@@ -9,25 +23,36 @@ router = APIRouter(
 )
 
 
-@router.get("/{audio_id}")
+@router.get("{audio_id}")
 async def get_audio(audio_id: int):
     print(f"Got audio with ID: {audio_id}")
     return {"test": audio_id}
 
 
-@router.get("/")
+@router.get("")
 async def get_all_audios():
     print("Printed all audios")
     return {"test": "test"}
 
 
-@router.post("/")
-async def post_new_audio():
-    print("Created new audio")
-    return {"test": "test"}
+@router.post("", responses={400: {"description": "Invalid file"}})
+async def post_new_audio(id: UUID4, file: UploadFile, db: Session = Depends(get_db)):
+    try:
+        y_stereo, sr = librosa.load(BytesIO(file.file.read()), sr=None, mono=False)
+    except:
+        raise HTTPException(status_code=400, detail="Invalid file")
+    duration = round(librosa.get_duration(y=y_stereo, sr=sr), 2)
+    channels = len(y_stereo)
+    params = Audios(
+        id=id, audio_length=duration, channels=channels, url="test", frequency=int(sr)
+    )
+    create_audio(db=db, audio=params)
+    return {
+        "test": f"duration: {duration} seconds and amount of channels is equal to {channels} "
+    }
 
 
-@router.delete("/{audio_id}")
+@router.delete("{audio_id}")
 async def remove_audio(audio_id: int):
     print(f"Removed audio with ID: {audio_id}")
     return {"test": audio_id}
