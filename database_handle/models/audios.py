0a2a0c875af3e11b8ed0cac5404d9cbaf8a3805a
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-07-01 10:55:09 +0200

    Added file_name given url is based on uuid

--- database_handle/models/audios.py
+++ database_handle/models/audios.py
@@ -7,6 +7,7 @@ class Audios(Base):
 
     id = Column(Uuid, primary_key=True, index=True)
     url = Column(String, nullable=False, unique=True)
+    file_name = Column(String, nullable=False)
     channels = Column(Integer, nullable=False)
     frequency = Column(Integer, nullable=False)
     audio_length = Column(Float, nullable=False)
