5febbbc0f8690911ae3c364f467ccb3d134e8606
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-07-02 16:20:10 +0200

    Added commit endpoint

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+from uuid import uuid4
 from fastapi.responses import RedirectResponse
-from database_handle.database import engine
-from fastapi import FastAPI
+from sqlalchemy.orm import Session
+from database_handle.database import SessionLocal, engine, get_db
+from fastapi import Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from database_handle.models import texts, audios, categories, bindings
+from database_handle.queries.categories import create_category, get_categories_count
 from routes import (
     categories as r_categories,
     texts as r_texts,
@@ -15,6 +18,7 @@ audios.Base.metadata.create_all(engine)
 categories.Base.metadata.create_all(engine)
 bindings.Base.metadata.create_all(engine)
 origins = "https?://localhost:.+"
+
 
 app = FastAPI()
 # Dependency
@@ -36,3 +40,8 @@ app.include_router(r_bindings.router)
 @app.get("/")
 def root():
     return RedirectResponse("http://localhost:8000/docs")
+
+
+@app.get("/commit")
+def commit(db: Session = Depends(get_db)):
+    db.commit()
