ec11c4c216ea8b71b71507298c82c9ed97bb2f52
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-07-02 16:20:00 +0200

    Made creating and removing categories working

--- routes/categories.py
+++ routes/categories.py
@@ -4,7 +4,12 @@ from sqlalchemy.orm import Session
 
 from database_handle.database import get_db
 from database_handle.models.categories import Categories
-from database_handle.queries.categories import create_category
+from database_handle.queries.categories import (
+    create_category,
+    get_all_categories as all_categories_query,
+    get_one_category,
+    remove_category as category_delete,
+)
 
 __all__ = ["router"]
 
@@ -15,39 +20,37 @@ router = APIRouter(
 )
 
 
-@router.get("/{category_id}")
-async def get_category(category_id: int):
-    print(f"Got category with ID: {category_id}")
-    return {"test": category_id}
+@router.get("")
+async def get_all_categories(db: Session = Depends(get_db)):
+    return all_categories_query(db)
 
 
-@router.get("/")
-async def get_all_categories():
-    print("Got all categories")
-    return {"test": "test"}
-
-
-@router.post("/")
+@router.post("")
 async def post_new_category(
     id: UUID4 = Form(), category: str = Form(), db: Session = Depends(get_db)
 ):
+    res = get_one_category(db=db, name=category)
+    if res is not None:
+        db.rollback()
+        raise HTTPException(
+            status_code=400, detail=f"Category '{category}' already exists"
+        )
     new_category = Categories(id=id, name=category)
     try:
         create_category(db=db, category=new_category)
     except Exception as e:
-        raise HTTPException(
-            status_code=400, detail=f"Category {category} already exists"
-        )
+        pass
+    db.commit()
+
     return {"test": "test"}
 
 
-@router.patch("/{category_id}")
-async def update_category(category_id: int):
-    print(f"Updated category with name: {category_id}")
-    return {"test": category_id}
+@router.patch("/{category_name}")
+async def update_category(category_name: str, new_category_name: str = Form()):
+    pass
 
 
-@router.delete("/{category_id}")
-async def remove_category(category_id: int):
-    print(f"Removed category with ID: {category_id}")
-    return {"test": category_id}
+@router.delete("/{category_name}")
+async def remove_category(category_name: str, db: Session = Depends(get_db)):
+    res = category_delete(db=db, name=category_name)
+    return res
