b7af90c7a25fc7ae51a64cdbc546f458ec9506f4
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-07-02 16:19:37 +0200

    Moved creating category into bindings

--- routes/bindings.py
+++ routes/bindings.py
@@ -3,13 +3,22 @@ from typing import Annotated
 from sqlalchemy.orm import Session
 from uuid import uuid4
 from database_handle.database import get_db
+from database_handle.models.audios import Audios
 from database_handle.models.bindings import Bindings
+from database_handle.models.categories import Categories
 from database_handle.queries.bindings import (
     get_one_binding,
     create_binding as create_new_binding,
+    get_total_bindings,
+    get_paginated_bindings as paginated_bindings_query,
+    get_all_bindings as all_bindings_query,
 )
 import asyncio
-from database_handle.queries.categories import get_one_category
+from database_handle.queries.categories import (
+    create_category,
+    get_categories_count,
+    get_one_category,
+)
 from routes.audios import post_new_audio
 from routes.categories import post_new_category
 from routes.texts import post_new_text
@@ -23,9 +32,21 @@ router = APIRouter(
 )
 
 
+@router.get("/count")
+def get_count(db: Session = Depends(get_db)):
+    return get_total_bindings(db) or 0
+
+
 @router.get("")
-def get_all_bindings():
-    return {"hejo", "hejo"}
+def get_paginated_bindings(
+    page: int = 1, per_page: int = 10, db: Session = Depends(get_db)
+):
+    return paginated_bindings_query(page=page, limit=per_page, db=db)
+
+
+@router.get("/all")
+def get_all_bindings(db: Session = Depends(get_db)):
+    return all_bindings_query(db)
 
 
 @router.get("/{binding_id}")
@@ -46,16 +67,17 @@ async def create_binding(
     new_binding = Bindings(
         id=binding_id, category_id=category_id, audio_id=binding_id, text_id=binding_id
     )
+    new_category = Categories(id=id, name=category)
     try:
         await asyncio.gather(
             *[
                 post_new_audio(id=binding_id, file=audio, db=db),
-                post_new_category(id=category_id, category=category, db=db),
                 post_new_text(id=binding_id, text=text, db=db),
             ]
         )
 
         create_new_binding(db=db, binding=new_binding)
+        create_category(db=db, category=new_category)
     except Exception as e:
         db.rollback()
         raise HTTPException(status_code=400, detail=str(e))
