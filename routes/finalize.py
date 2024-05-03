6bf0e39f063027cdcf88010eb8e7eb222fa503d2
Herob527 <szymon.wrzos@onet.pl>
2024-05-04 00:42:30 +0200

    Implemented finalization

--- routes/finalize.py
+++ routes/finalize.py
@@ -1,12 +1,18 @@
-from collections.abc import Callable
+from collections.abc import Callable, Sequence
 from pathlib import Path
-from shutil import copy2
+from shutil import copy2, rmtree
+from typing import Dict, Tuple, TypedDict
 
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel
+from sqlalchemy import Row
 from sqlalchemy.orm import Session
 
 from database_handle.database import get_db
+from database_handle.models.audios import Audio
+from database_handle.models.bindings import Binding
+from database_handle.models.categories import Category
+from database_handle.models.texts import Text
 from database_handle.queries.bindings import get_all_bindings
 
 __all__ = ["router"]
@@ -19,10 +25,11 @@ class FinaliseConfigModel(BaseModel):
     line_format: str = "{file}|{text}"
     divide_by_category: bool = True
     export_transcript: bool = True
+    uncaterized_name: str = "Uncategorized"
 
 
 def prepare_path(dir: str):
-    path = Path(dir)
+    path = Path(output_dir, dir, "wavs")
     path.mkdir(parents=True, exist_ok=True)
     return path
 
@@ -39,7 +46,7 @@ def write_transcript(
     target_file: str,
     filter_predicate: Callable[[str], bool] = lambda _: True,
 ):
-    with open(target_file, "w", encoding="utf-8") as f:
+    with open(target_file, "a", encoding="utf-8") as f:
         f.writelines(filter(filter_predicate, lines))
 
 
@@ -49,14 +56,91 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
+type BindingEntry = Row[Tuple[Binding, Category, Audio, Text]]
+
+
+def process_path(_binding: BindingEntry, config: FinaliseConfigModel):
+    _, category, audio, _ = _binding.tuple()
+
+    target_dir = (
+        Path(
+            output_dir,
+            str(category.name) if category is not None else config.uncaterized_name,
+        )
+        if config.divide_by_category
+        else output_dir
+    )
+
+    output_file = Path(target_dir, "wavs", str(audio.file_name))
+    source_file = Path(str(audio.url))
+
+    return [source_file, output_file]
+
+
+class TranscriptEntry(TypedDict):
+    lines: list[str]
+    path: Path
+
+
+def process_transcript(bindings: Sequence[BindingEntry], config: FinaliseConfigModel):
+    res: Dict[str, TranscriptEntry] = dict()
+    for binding in bindings:
+        _, category, audio, text = binding.tuple()
+        target_dir = (
+            Path(
+                output_dir,
+                str(category.name) if category is not None else config.uncaterized_name,
+            )
+            if config.divide_by_category
+            else output_dir
+        )
+
+        output_file = Path(target_dir, "transcript.txt")
+        current_category = (
+            str(category.name) if category is not None else config.uncaterized_name
+        )
+
+        current_value = res.get(current_category)
+        text_to_insert = f"{audio.file_name}|{text.text}\n"
+        if current_value is None:
+            res[current_category] = {
+                "lines": [text_to_insert],
+                "path": output_file,
+            }
+        else:
+            res[current_category]["lines"].append(text_to_insert)
+
+    return res.values()
+
 
 @router.post("/")
 def finalise(config: FinaliseConfigModel, db: Session = Depends(get_db)):
+    rmtree(output_dir, ignore_errors=True)
+    output_dir.mkdir()
     bindings = get_all_bindings(db)
     categories = (
-        set(map(lambda x: x.tuple()[1].name.name, bindings))
+        map(
+            lambda x: (
+                str(x.tuple()[1].name) if x.tuple()[1] else config.uncaterized_name
+            ),
+            bindings,
+        )
         if config.divide_by_category
-        else ["all"]
+        else map(lambda _: "All", ["all"])
     )
+    audio_paths = map(lambda x: process_path(x, config), bindings)
+
+    transcript_data = process_transcript(bindings, config)
+
     for category in categories:
         prepare_path(category)
+
+    for audio_path in audio_paths:
+        copy_file(str(audio_path[0]), str(audio_path[1]))
+
+    for data in transcript_data:
+        write_transcript(
+            data["lines"],
+            str(data["path"]),
+            lambda x: not x.strip().endswith("|") if config.omit_empty else True,
+        )
