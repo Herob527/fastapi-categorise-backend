1e381bf5e5c54efa4716365828e710edd7e06108
Herob527 <szymon.wrzos@onet.pl>
2025-05-24 17:49:51 +0200

    Added route

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -10,7 +10,13 @@ Create endpoint for dashboard
 - Attach categories grouped by amounts attached
 """
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm.session import Session
+
+from database_handle.database import get_db
+from database_handle.models.dashboard import DashboardModel
+from database_handle.queries.dashboard import get_categories_count, get_categorized_count, get_category_with_most_bindings, get_empty_transcript_count, get_filled_transcript_count, get_total_audio_duration, get_total_bindings_count, get_uncategorized_count
+
 
 __all__ = ["router"]
 
@@ -19,3 +25,24 @@ router = APIRouter(
     prefix="/dashboard",
     responses={404: {"description": "Not found"}},
 )
+@router.get("/", response_model=DashboardModel)
+async def get_dashboard(db: Session = Depends(get_db)):
+    categories_count = get_categories_count(db)
+    total_bindings_count = get_total_bindings_count(db)
+    category_with_most_bindings = get_category_with_most_bindings(db)
+    uncategorized_count = get_uncategorized_count(db)
+    categorized_count = get_categorized_count(db)
+    total_audio_duration = get_total_audio_duration(db)
+    filled_transcript_count = get_filled_transcript_count(db)
+    empty_transcript_count = get_empty_transcript_count(db)
+
+    return DashboardModel(
+        categories_count=categories_count,
+        total_bindings_count=total_bindings_count,
+        category_with_most_bindings=category_with_most_bindings,
+        uncategorizaed_count=uncategorized_count,
+        categorized_count=categorized_count,
+        total_audio_duration=total_audio_duration,
+        filled_transcript_count=filled_transcript_count,
+        empty_transcript_count=empty_transcript_count
+    )
