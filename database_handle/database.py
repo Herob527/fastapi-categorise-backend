350c758c54848b633a35dd33684914bc9c2911d2
Herob527 <szymon.wrzos@onet.pl>
2025-05-25 13:14:32 +0200

    Changed url to use one from env variable

--- database_handle/database.py
+++ database_handle/database.py
@@ -1,11 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from os import environ
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+SQLALCHEMY_DATABASE_URL = environ.get("SQLALCHEMY_DATABASE_URL")
+
+if not SQLALCHEMY_DATABASE_URL:
+    raise ValueError("SQLALCHEMY_DATABASE_URL is not set")
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL,
 )
 
 SessionLocal = sessionmaker(bind=engine, autoflush=False)
