33608dec1139b8f568eecb0478c171095442fe72
Herob527 <szymon.wrzos@onet.pl>
2025-12-26 09:18:02 +0100

    Added pagination

--- database_handle/models/bindings.py
+++ database_handle/models/bindings.py
@@ -37,6 +37,13 @@ class Binding(Base):
 
 class PaginationModel(BaseModel):
     total: int
+    current_page: int
+    total_pages: int
+    per_page: int
+    has_next: bool
+    has_previous: bool
+    next_page: int | None = None
+    previous_page: int | None = None
 
 
 class BindingEntry(BaseModel):
