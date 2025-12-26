7b716f9e2fbd189c641fe7f8b06a26e803b938cb
Herob527 <szymon.wrzos@onet.pl>
2025-12-26 11:39:35 +0100

    Working on tracking exports

---
+++ database_handle/models/exports.py
@@ -0,0 +1,19 @@
+import enum
+from sqlalchemy import Column, Float, String, Enum
+from ..database import Base
+
+
+class ExportStatus(enum.Enum):
+    PENDING = 0
+    IN_PROGRESS = 1
+    COMPLETED = 2
+    FAILED = 3
+
+
+class Exports(Base):
+    __tablename__ = "exports"
+
+    id = Column(String, primary_key=True, index=True)
+    status = Column(Enum(ExportStatus), default=ExportStatus.PENDING)
+    created_at = Column(Float, nullable=False)
+    updated_at = Column(Float, nullable=False)
