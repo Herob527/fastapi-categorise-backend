c45ed4405d90e9cb663903f60a39315e025a01f9
Herob527 <szymon.wrzos@onet.pl>
2026-05-01 21:37:35 +0200

    Remove redundant stuff

--- database_handle/jobs/listen_to_minio.py
+++ database_handle/jobs/listen_to_minio.py
@@ -1,9 +1,6 @@
 import asyncio
 from typing import TypedDict
-from database_handle.models.audios import StatusEnum
-from database_handle.queries.audios import AudioQueries
 from services import minio_service
-from database_handle.database import get_sessionmanager
 
 service = minio_service.minio_service
 
@@ -38,23 +35,8 @@ class MinioEvent(TypedDict):
     Records: list[MinioRecord]
 
 
-async def _handle_record(uuid: str) -> None:
-    async with get_sessionmanager().session() as session:
-        await AudioQueries(session).update_audio(
-            audio_id=uuid, status=StatusEnum.available
-        )
-
-
 def listen_to_minio(loop: asyncio.AbstractEventLoop) -> None:
     for event in service.listen_to_bucket():
         minio_event: MinioEvent = event
         for record in minio_event.get("Records", []):
             event_name = record["eventName"]
-            key = record["s3"]["object"]["key"]
-            print(f"{key=}")
-            print(f"{event_name=}")
-            uuid = record["s3"]["object"]["userMetadata"]["X-Amz-Meta-Uuid"]
-            print(uuid)
-            if event_name.startswith(EVENT_OBJECT_CREATED):
-                future = asyncio.run_coroutine_threadsafe(_handle_record(uuid), loop)
-                future.result()
