148431681b5b8e11a199835df710bb5bd478544a
Herob527 <szymon.wrzos@onet.pl>
2026-07-26 23:27:02 +0200

    Revert "Working out dehardcode"
    This reverts commit 207343d10d766b2203c8825dc328bdbbf932beb7.

--- routes/finalize/constants.py
+++ routes/finalize/constants.py
@@ -5,11 +5,3 @@ OUTPUT_DIR = Path("temp")
 OUTPUT_ARCHIVE = "categorized_files.zip"
 
 EMPTY_TEXT_TAG = "<empty-text>"
-
-
-class TranscriptFile:
-    name = "transcript.txt"
-
-    @classmethod
-    def as_path(cls, *parts: Path | str) -> Path:
-        return Path(*parts, cls.name)
